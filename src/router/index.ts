f1ea18096ad033c504d6572df207ea6478669f27
nicolefemello <nicfmello@gmail.com>
2025-11-10 10:52:36 -0300

    feat: add seat selection interface for movie tickets

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      component: () => import('../layouts/Default.vue'),
+      component: () => import('../layouts/DefaultLayout.vue'),
       children: [
         {
           path: '/',
