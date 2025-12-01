aa34783c4f8ec6a7b460478dcacab92f3676753c
nicolefemello <nicfmello@gmail.com>
2025-12-01 11:00:07 -0300

    STYLE: format header component template and update import path

--- src/components/general/headerComp.vue
+++ src/components/general/headerComp.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import type { IMenu } from '@/interfaces/menuInterface'
+import type { IMenu } from '@/interfaces/general'
 import { Notifications } from '@/components'
 
 const menu: IMenu[] = [{ icon: 'notifications' }, { icon: 'person', link: '/profile' }]
@@ -13,44 +13,26 @@ const togglePopup = () => {
 
 <template>
   <header>
-    <nav
-      class="hidden lg:flex justify-between items-center px-20 py-4 bg-[#252525] text-white font-semibold"
-    >
+    <nav class="hidden lg:flex justify-between items-center px-20 py-4 bg-[#252525] text-white font-semibold">
       <ul class="flex items-center gap-4">
         <li>
-          <RouterLink
-            to="/"
-            class="bg-gradient-to-r from-[rgb(255,0,85)] to-[#990033] bg-clip-text text-transparent text-3xl font-bold"
-          >
-            CINE LUMIÉRE</RouterLink
-          >
+          <RouterLink to="/"
+            class="bg-gradient-to-r from-[rgb(255,0,85)] to-[#990033] bg-clip-text text-transparent text-3xl font-bold">
+            CINE LUMIÉRE</RouterLink>
         </li>
       </ul>
 
       <ul class="flex items-center gap-4">
         <li v-for="(item, index) in menu" :key="index">
-          <span
-            v-if="item.icon == 'notifications'"
-            @click="togglePopup"
-            class="material-symbols-outlined cursor-pointer py-2"
-            >{{ item.icon }}</span
-          >
-          <Notifications
-            v-if="item.icon == 'notifications'"
-            v-show="showPopup"
-            @close="togglePopup"
-          />
+          <span v-if="item.icon == 'notifications'" @click="togglePopup"
+            class="material-symbols-outlined cursor-pointer py-2">{{ item.icon }}</span>
+          <Notifications v-if="item.icon == 'notifications'" v-show="showPopup" @close="togglePopup" />
 
           <RouterLink v-else :to="item.link">
-            <span
-              class="material-symbols-outlined cursor-pointer py-2"
-              :class="
-                item.icon == 'person'
-                  ? 'bg-gradient-to-l from-[rgb(255,0,85)] to-[#990033] px-2 rounded-lg hover:scale-110 transition-all'
-                  : ''
-              "
-              >{{ item.icon }}</span
-            >
+            <span class="material-symbols-outlined cursor-pointer py-2" :class="item.icon == 'person'
+                ? 'bg-gradient-to-l from-[rgb(255,0,85)] to-[#990033] px-2 rounded-lg hover:scale-110 transition-all'
+                : ''
+              ">{{ item.icon }}</span>
           </RouterLink>
         </li>
       </ul>
